0271b11abe4282a633473573c69b1674d3d421c9
Monsieur <2toto78@gmail.com>
2026-02-04 16:14:01 +0100

    few changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://thomaslfb.eu', // your domain
   base: '/',                     // keep '/' unless you deploy in a subfolder
+  integrations: [sitemap()]
 });
