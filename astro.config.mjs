b75bdb47735e4eb3d160906ea19ae76f0cde1139
Monsieur <2toto78@gmail.com>
2026-02-11 16:06:52 +0100

    add robot

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://thomaslfb.eu', // your domain
+  site: 'https://www.thomaslfb.eu', // your domain
   base: '/',                     // keep '/' unless you deploy in a subfolder
   integrations: [sitemap()]
 });
